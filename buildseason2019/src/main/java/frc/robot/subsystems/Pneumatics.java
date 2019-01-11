e8b26a8529d7bbc24107973e178070c3823fda27
Garrett Hart <garrett7621@gmail.com>
2019-01-11 17:46:07 -0500

    creates pnuematics subsystem

---
+++ buildseason2019/src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -0,0 +1,28 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Solenoid;
+
+public class Pneumatics extends Subsystem{
+
+    Compressor compressor;
+    Solenoid leftHSelenoid;
+    Solenoid rightHSelenoid;
+
+    public Pneumatics(){
+        compressor = new Compressor(0);
+        compressor.setClosedLoopControl(true);
+    }
+
+    public setHWheelSelenoids(boolean val){
+        System.out.println("setting H Wheel Selenoids " + val);
+        leftHSelenoid.set(val);
+        rightHSelenoid.set(val);
+    }
+	@Override
+	protected void initDefaultCommand() {
+		
+	}
+
+}
