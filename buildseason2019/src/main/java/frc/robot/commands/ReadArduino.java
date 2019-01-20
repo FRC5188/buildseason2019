c665d90c7e5b5bca992319675fd82ca1a9bc6a84
Garrett Hart <garrett7621@gmail.com>
2019-01-20 15:49:51 -0500

    creates comand for reading arduino data

---
+++ buildseason2019/src/main/java/frc/robot/commands/ReadArduino.java
@@ -0,0 +1,54 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
+
+public class ReadArduino extends Command {
+
+    private String arduinoData = "none";
+    private boolean isFinished = false;
+
+    public ReadArduino() {
+        // Use requires() here to declare subsystem dependencies
+        requires(Robot.i2c);
+    }
+
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+        arduinoData = Robot.i2c.read();
+        System.out.println("Data from Arduino: " + arduinoData);
+        isFinished = true;
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished() {
+        return isFinished;
+    }
+
+    // Called once after isFinished returns true
+    @Override
+    protected void end() {
+
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    @Override
+    protected void interrupted() {
+
+    }
+}
