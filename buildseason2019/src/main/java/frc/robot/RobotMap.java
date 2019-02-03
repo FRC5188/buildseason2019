9c29240c4c42f7e39ab6925b054d36fccb6bed27
Garrett Hart <garrett7621@gmail.com>
2019-02-02 21:03:35 -0500

    message

--- buildseason2019/src/main/java/frc/robot/RobotMap.java
+++ buildseason2019/src/main/java/frc/robot/RobotMap.java
@@ -8,22 +8,22 @@
 package frc.robot;
 
 
-import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.SerialPort;
-import edu.wpi.first.wpilibj.I2C;
-import edu.wpi.first.wpilibj.I2C.Port;
-import edu.wpi.first.wpilibj.Compressor;
+        import com.kauailabs.navx.frc.AHRS;
+        import edu.wpi.first.wpilibj.SerialPort;
+        import edu.wpi.first.wpilibj.I2C;
+        import edu.wpi.first.wpilibj.I2C.Port;
+        import edu.wpi.first.wpilibj.Compressor;
 
 
 //basic motor mappings for h-drrive
 public class RobotMap {
-  
+
   //Everything on the CAN bus has an ID
   //The default ID for only one item on the can bus is 0
   public static final int COMPRESSOR_CAN_ID = 0;
 
   public static Compressor compressor = new Compressor(COMPRESSOR_CAN_ID);
-  
+
   public static final int frontLeft = 0;
   public static final int frontRight = 2;
   public static final int backLeft = 1;
@@ -35,6 +35,6 @@ public class RobotMap {
 
   public static final int HSolenoid = 0;
 
-  public static AHRS gyro = new AHRS(SerialPort.Port.kMXP); 
+  public static AHRS gyro = new AHRS(SerialPort.Port.kMXP);
   public static I2C wire = new I2C(Port.kMXP, 4); //uses the i2c port on the RoboRIO
 }
