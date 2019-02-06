e8b6d3b63bd79cdc99bed2cae6c20aba6222a3d9
Garrett Hart <garrett7621@gmail.com>
2019-02-05 20:13:24 -0500

    temp

--- buildseason2019/src/main/java/frc/robot/Robot.java
+++ buildseason2019/src/main/java/frc/robot/Robot.java
@@ -128,6 +128,6 @@ public class Robot extends TimedRobot {
   private void log() {
       SmartDashboard.putNumber("Gyro Angle", RobotMap.gyro.getAngle());
       SmartDashboard.putData("Scheduler", Scheduler.getInstance());
-      //The scheduler will show running commands
+      //The scheduler will show all running commands
   }
 }
