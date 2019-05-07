5d23c35b2812b475ad9726bb6ba6c2309698e280
garrett928 <garrett7621@gmail.com>
2019-05-07 17:10:17 -0400

    starts cleaning up dive command

--- buildseason2019/src/main/java/frc/robot/commands/driveTrain/Drive.java
+++ buildseason2019/src/main/java/frc/robot/commands/driveTrain/Drive.java
@@ -15,35 +15,44 @@ import frc.robot.subsystems.I2C;
 
 public class Drive extends Command {
 
-	//we orgianally had h-drive but now have 6 wheel tank...
+    /**
+     *
+     */
 
 	I2C wire = Robot.i2c;
 
+	/*Constructor*/
 	public Drive() {
 		requires(Robot.driveTrain);
 		SmartDashboard.putBoolean("Drive Running", false);
-		//bool to let us see if drive or pixydrive is running
+		/*bool to let us see if drive or PixyDrive is running*/
 	}
 
 	@Override
 	protected void initialize() {
+		/*Allows the PixyDrive command to interrupt and take over*/
 		setInterruptible(true);
-		//allows the pixyDrive command to interrupt and take over
 	}
 
 	@Override
 	protected void execute() {
-        //get all of the driver's joystick values
+        /*get all of the driver's joystick values*/
+
 		double throttle = OI.drive.getRawAxis(OI.Axis.LY);
 		double turn =  OI.drive.getRawAxis(OI.Axis.RX);
 		double quickTurn = OI.drive.getRawAxis(OI.Axis.LTrigger);
 		boolean shifter = OI.drive.getRawButton(OI.Buttons.R);
-		//actual driveTrain method
-		//Robot.driveTrain.driveArcade(throttle, turn, 0, shifter);
-        Robot.driveTrain.cheesyDrive(throttle, turn,quickTurn, shifter);//is called during normal drive, as well as pixy, so that we
-		//can see if the robot can see the tape without having to enable the pixydrive
 
-		//this makes the overall driving experience for the driver easier and more efficent 
+        // TODO: 5/6/2019 make sperate drive commands for arcade drive and cheesy drive
+
+		//Robot.driveTrain.driveArcade(throttle, turn, 0, shifter);
+        Robot.driveTrain.cheesyDrive(throttle, turn,quickTurn, shifter);
+
+        /*
+          Calling "isTape()" will get the Pixy data from the arduino and allow the
+          driver and operator to see on the dashboard if the robot currently is
+          able to see any vision targets
+        */
 		wire.isTape();
 		SmartDashboard.putBoolean("Drive Running", true);
 	}
@@ -51,7 +60,7 @@ public class Drive extends Command {
 	@Override
 	protected boolean isFinished() {
 		/*
-		 * driveTrain command should always run but, should be incorruptible so other
+		 * driveTrain command should always run but, should be interruptable so other
 		 * commands can control the drivetrain
 		 */
 		return false;
@@ -59,14 +68,15 @@ public class Drive extends Command {
 
 	@Override
 	protected void end() {
+		/*stops motors when operator control is finished, notifies dashboard*/
+
 		SmartDashboard.putBoolean("Drive Running", false);
 		Robot.driveTrain.stop();
-		// stops motors when command is finished
 	}
 
 	@Override
 	protected void interrupted() {
+		/*It is normal for commands to just call end*/
 		this.end();
-		// ends command when interrupted
 	}
 }
