58b7b43ad8b16fa9b4c23d41c2dbd48704cd7e46
garrett928 <garrett7621@gmail.com>
2019-05-16 14:25:15 -0400

    clean up cargo code

--- buildseason2019/src/main/java/frc/robot/commands/OperateCargoIntake.java
+++ buildseason2019/src/main/java/frc/robot/commands/OperateCargoIntake.java
@@ -6,6 +6,11 @@ import frc.robot.Robot;
 
 public class OperateCargoIntake extends Command {
 
+    /*
+    * This command runs our cargo intake. The intake only consist of
+    * a set of wheels, powered by a 775 pro, sitting on top of the hatch intake.
+    **/
+
     public OperateCargoIntake(){
         this.requires(Robot.intake);
     }
@@ -17,21 +22,23 @@ public class OperateCargoIntake extends Command {
 
     @Override
     protected void execute(){
-        double intakeMotorPower;
-        intakeMotorPower = OI.operator.getRawAxis(OI.Axis.RTrigger) - OI.operator.getRawAxis(OI.Axis.LTrigger);
-        //use the left and right triggers on the logitech controller for intaking the cargo
+        /*use the left and right triggers on the logitech controller for intaking the cargo*/
+        double intakeMotorPower = OI.operator.getRawAxis(OI.Axis.RTrigger) -
+                                  OI.operator.getRawAxis(OI.Axis.LTrigger);
 
+        /*create a small deadband for the motor power*/
         if(Math.abs(intakeMotorPower) < .01) intakeMotorPower = 0;
-        //create a small deadband for the motor power
 
+        /*set intake power*/
         Robot.intake.setIntakeMotor(intakeMotorPower);
-        //set intake power
     }
 
     @Override
     protected boolean isFinished() {
-        //this command never needs to finish since it's the only command
-        //using the cargo intake
+        /*
+          this command never needs to finish since it's the only command
+          using the cargo intake
+         */
         return false;
     }
 }
