1b27915423952cc5db4a5038b27abba2c1530579
garrett928 <garrett7621@gmail.com>
2019-05-16 14:37:02 -0400

    clean up d-pad button

--- buildseason2019/src/main/java/frc/robot/DPadButton.java
+++ buildseason2019/src/main/java/frc/robot/DPadButton.java
@@ -5,7 +5,12 @@ import edu.wpi.first.wpilibj.buttons.Button;
 
 public class DPadButton extends Button {
 
-    //custom button class to use the dpad buttons on a logitech or xbox controller
+    /*
+    * Custom button class to use the dpad buttons on a logitech or xbox controller
+    *
+    * This is not our original code. I believe it's on chief delphi but I don't
+    * have a link.
+    **/
     
     Joystick joystick;
     Direction direction;
@@ -15,12 +20,12 @@ public class DPadButton extends Button {
         this.direction = direction;
     }
 
-    public static enum Direction {
+    public enum Direction {
         UP(0), RIGHT(90), DOWN(180), LEFT(270);
 
         int direction;
 
-        private Direction(int direction) {
+         Direction(int direction) {
             this.direction = direction;
         }
     }
